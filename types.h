6a01cf905be2f39585805d12f13417dbb411e8c3
Priya <154120063+Priyasusi1@users.noreply.github.com>
2024-09-09 22:08:42 +0530

    Add files via upload

---
+++ types.h
@@ -0,0 +1,22 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned int uint;
+
+typedef enum
+{
+	e_success,
+	e_failure
+}Status;
+
+typedef enum
+{
+	e_view,
+	e_edit,
+	e_help,
+	e_unsupported
+}OperationType;
+
+#endif
+
+
